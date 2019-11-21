96a5427c2f187c15a2492a2db04306dc9e12bb91
The Dark Security <52529306+thedarksec@users.noreply.github.com>
2019-11-21 14:41:42 +0700

    Update anonymous.py

--- anonymous.py
+++ anonymous.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Anonymous
 # Recode by Senja
-# Github: github.com/thesixtynine/Anonymous
+# Github: github.com/thedarksec/Anonymous
 
 import os, sys, time, random, requests
 
@@ -11,9 +11,9 @@ time.sleep(1)
 print("\033[0m")
 print("\033[0mAnonymous\033[0m Email by anonymouse.org")
 print("The 4chan.org \033[1;4mHacktivist\033[0m Global Groups \n")
-to = raw_input('to: ')
-subject = raw_input('subject: ')
-message = raw_input('message: ')
+to = raw_input('\033[0m[\033[1;91m-\033[0m] \033[1;77mTo: \033[0m')
+subject = raw_input('\033[0m[\033[1;96m#\033[0m] \033[1;77mSubject: \033[0m')
+message = raw_input('\033[0m[\033[1;94m*\033[0m] \033[1;77mmessage: \033[0m')
 
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 sess = requests.Session()
@@ -34,5 +34,5 @@ email_req = sess.post('http://anonymouse.org/cgi-bin/anon-email.cgi', headers={
 })
 
 if 'The e-mail has been sent' in email_req.text:
-    print("[+] Email Sent!")
-    print("[+] In order to increase your privacy, the anonymous e-mail will be randomly delayed up to 12 hours")
+    print("\033[0m[\033[1;93m+\033[0m] \033[1;77mEmail Sent!")
+    print("\033[0m[\033[1;92m!\033[0m] \033[1;77mIn order to increase your privacy, the anonymous e-mail will be randomly delayed up to 12 hours")
